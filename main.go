58baf6149449835bdb028ab98c1ea67f24072b75
Sergey Shopen <ShopinSergg@gmail.com>
2022-11-08 00:00:15 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Property struct {
+	GlobalId int `json:"global_id"`
+}
+
+type Response struct {
+	Sum int
+}
+
+func main() {
+	var (
+		properties []Property
+		result     int
+	)
+
+	file, _ := os.Open("data-20190514T0100.json")
+	dataFromFile, _ := io.ReadAll(file)
+
+	err := json.Unmarshal([]byte(dataFromFile), &properties)
+	if err != nil {
+		panic("Ошибка JSON")
+	}
+
+	result = getSumId(properties)
+
+	response := Response{
+		Sum: result,
+	}
+	data, _ := json.MarshalIndent(response, "", "    ")
+	fmt.Printf("%s", string(data))
+}
+
+func getSumId(properties []Property) int {
+	var count int
+	for _, property := range properties {
+		count += property.GlobalId
+	}
+	return count
+}
